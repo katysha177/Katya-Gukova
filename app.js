8489d4d7dae968dff972e1f1af8de54e4e3946e3
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-05-27 22:30:50 +0300

    <lessons2task2>

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-Привет
+const per = "hello world";
+console.log(per)
+const her = 3;
+const her2 = 5;
+const result = her + her2
+const result2 = her * her2
+const result3 = her2 - her
+const result4 = her2 / her2
+console.log(result,result2,result3,result4)
