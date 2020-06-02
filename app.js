912457bd4ac17696b3eab7251cede9d17202c0bc
katysha177 <65913619+katysha177@users.noreply.github.com>
2020-06-02 19:16:39 +0300

    'lesson12tast12new'

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ else { return console.log(li+10) }
 //task 12 start
 const _ = require('lodash');
 const tre = "i want go to walk";
-const registr = _.upperCase(tre);
+const registr = () => { const tre = "i want go to walk"; _.upperCase(tre);
 console.log(registr)
 const num = {num1:4, num2: 6};
-const f_num = (num1,num2) => { add(4,6) }
+const f_num = (num1,num2) => { add(num1,num2) }
 console.log(f_num)
 //task 12 end
